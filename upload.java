5df9854603b7da8851676d648411a5867d216e20
Ayush Gupta <ag7159441@gmail.com>
2022-03-20 21:39:52 +0530

    Add files via upload

---
+++ upload.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class upload{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        long[] ar=new long[n];
+        for(int i=0;i<n;i++)
+        ar[i]=sc.nextInt();
+        long count=0;
+        for(int i=1;i<n;i++)
+        {
+            if(ar[i]<ar[i-1])
+            {
+                count+=ar[i-1]-ar[i];
+                ar[i]=ar[i-1];
+            }
+        }
+        System.out.println(count);
+    }
+}
